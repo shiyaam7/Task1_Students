843a3ee6aa07dfbc5b724914742e2f7c5424fd88
Shiyaam J O <iamshiyaaam7@gmail.com>
2023-11-13 02:03:47 +0530

    final commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Class Sorting Application
+
+This is a web application that allows teachers to manage and sort their students in different classes using a drag-and-drop interface.
+
+## Features
+
+- Display a list of students for each class.
+- Allow teachers to rearrange the order of students using drag-and-drop.
+- Automatically update the sort order on the server.
+- Support multiple classes with independent sorting.
+
+## Technologies Used
+
+- HTML, CSS, JavaScript (Client-side)
+- Node.js, Express.js (Server-side)
+- MySQL (Database)
--- server.js
+++ server.js
@@ -22,19 +22,6 @@ connection.connect((err) => {
   console.log('Connected to MySQL');
 });
 
-let students = [
-    { class: 'Class A', name_of_student: 'Arun', sort_rank: 1 },
-    { class: 'Class B', name_of_student: 'Tejas', sort_rank: 1 },
-    { class: 'Class B', name_of_student: 'David', sort_rank: 2 },
-    { class: 'Class A', name_of_student: 'Uma', sort_rank: 2 },
-    { class: 'Class B', name_of_student: 'Divya', sort_rank: 3 },
-    { class: 'Class A', name_of_student: 'Simon', sort_rank: 3 },
-    { class: 'Class A', name_of_student: 'Rakesh', sort_rank: 4 },
-    { class: 'Class B', name_of_student: 'Selvam', sort_rank: 4 },
-    { class: 'Class B', name_of_student: 'Shruthi', sort_rank: 5 },
-    { class: 'Class A', name_of_student: 'Deepak', sort_rank: 5 },
-  ];
-
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname));
@@ -56,9 +43,6 @@ app.post('/updateSortOrder', (req, res) => {
   const updatedOrder = req.body.sortOrder;
 
   updatedOrder.forEach((student, index) => {
-    const studentIndex = students.findIndex(s => s.name_of_student === student);
-    students[studentIndex].sort_rank = index + 1;
-
     const query = 'UPDATE students SET sort_rank = ? WHERE class = ? AND name_of_student = ?';
     connection.query(query, [index + 1, 'Class A', student], (err) => {
       if (err) {
